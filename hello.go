bfc653d523cee6827eb853c1c6d8fd83bc44e673
Tyler Pearson <tyler.a.pear@gmail.com>
2025-11-26 18:45:40 -0800

    adds conditionals

--- hello.go
+++ hello.go
@@ -4,7 +4,16 @@ import "fmt"
 
 func main() {
 	var name string
+	var age int
 	fmt.Print("What is your name?: ")
 	fmt.Scanln(&name)
 	fmt.Printf("Hello %v!\n", name)
+	fmt.Print("What is your age?: ")
+	fmt.Scanln(&age)
+	
+	if age >= 18 {
+		fmt.Println("Congrats! You're an adult!")
+	} else {
+        fmt.Println("You're still a kid")
+    }
 }
